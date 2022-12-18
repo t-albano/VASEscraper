6db9216b38e8d5463d067467a0e0c0ae542957ed
t-albano <trenten.albano@gmail.com>
2022-12-18 08:44:36 -0600

    Works. Collected all entries since 2017. Look at site's layout from before 2017 to get more artwork (ideally, all the way back to 2009)

--- main.py
+++ main.py
@@ -7,28 +7,39 @@ import re
 import pandas as pd
 
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def up_to_years(current: int) -> list:
     # Get all years until last competition year from first competition year
     all_years = list()
-    first_year = 2001
+    first_year = 2017
     for i in range(first_year, current + 1):
         all_years.append(i)
 
     return all_years
 
 
+def total_art() -> int:
+    # find total number of art pieces in search looking at top of page
+    num_art = 0
+    h = driver.find_element(By.TAG_NAME, 'p').text
+    for i in h.split():
+        if i.isdigit():
+            num_art = int(i)
+            break
+
+    return num_art
+
+
 def pretty_data(phrase: str) -> list:
     # get messy string and put into standard format
-    cut_up = re.split('\n|Division:|Region:|  ', phrase)
+    cut_up = re.split('\n|Division:|Region:|   ', phrase)
     # format the info into something workable
-    for word in cut_up:
-        word.lstrip(" ")
-        word.rstrip(" ")
+    i = 0
+    while i < len(cut_up):
+        cut_up[i] = cut_up[i].strip(' ')
+        if cut_up[i] == '':
+            cut_up.pop(i)
+            i -= 1
+        i += 1
 
     # list in order [name, title, region, division, school]
     return cut_up
@@ -47,7 +58,7 @@ def gold_status(url: str) -> int:
     return stat
 
 
-def window_info(page: str) -> list:
+def window_info(page: str, status: int, date: int) -> list:
     # switch to newly opened window with artwork info
     driver.switch_to.window(page)
 
@@ -58,38 +69,66 @@ def window_info(page: str) -> list:
     g = gold_status(driver.current_url)
     artist_info.append(g)
 
+    if status == 1:
+        artist_info.append('2D')
+    else:
+        artist_info.append('3D')
+
+    artist_info.append(date)
+
     # list in the form [name, title, division, region, school, gold status]
     return artist_info
 
 
-# create driver. Will open new browser window.
-driver = webdriver.Chrome(ChromeDriverManager().install())
+if __name__ == "__main__":
+    # collect artwork id's on each page. Better than clicking on every thumbnail to get information
+    ids = list()
+    columns = ['Student', 'Title', 'Division', 'Region', 'School', 'Gold_Seal', 'Dimension', 'Year']
+    # create list of years up until now
+    years = up_to_years(2022)
 
-# url for VASE. Will append address to this to go through the year
-search_url = 'https://www.taea.org/vase/Search-2022.cfm'
+    for y in years:
+        search_url = f"https://www.taea.org/vase/Search-{y}.cfm"
 
-# create list of years up until now
-years = up_to_years(2022)
+        # create driver. Will open new browser window.
+        driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# Go to page
-driver.get(search_url)
+        for d in [1, 2]:
+            # Go to page
+            driver.get(search_url)
 
-# Select 2D in drop-down form
-dimension = Select(driver.find_element(By.NAME, 'Dimension'))
-dimension.select_by_index(2) # 1 for 2D, 2 for 3D
-# Click search
-driver.find_element(By.XPATH, "//input[@type='submit']").click()
+            # Select 2D in drop-down form
+            dimension = Select(driver.find_element(By.NAME, 'Dimension'))
+            dimension.select_by_index(d) # 1 for 2D, 2 for 3D
 
-# collect artwork id's on each page. Better than clicking on every thumbnail to get information
-ids = list()
+            # Click submit for search
+            driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
-main_hand = driver.current_window_handle
+            art_amount = total_art()
 
-driver.find_elements(By.XPATH, "//td[@class = 'artworkThumbnail']/a[@href]")[3].click()
-hand = driver.window_handles
+            while art_amount > 0:
 
-ids.append(window_info(hand[1]))
-driver.close()
+                main_hand = driver.current_window_handle
+                page_art = driver.find_elements(By.XPATH, "//td[@class = 'artworkThumbnail']/a[@href]")
 
+                art_amount -= len(page_art)
 
+                for view in page_art:
+                    view.click()
+                    hand = driver.window_handles
+
+                    # click on newly opened handle
+                    ids.append(window_info(hand[1], d, y))
+                    driver.close()
+                    driver.switch_to.window(main_hand)
+
+                if art_amount > 0:
+                    # click on next button conditional on number of artworks
+                    driver.find_element(By.XPATH, "//input[@src = '/VASE/images/next1.gif']").click()
+
+    driver.quit()
+
+    # export collected data into csv file for manipulation in R
+    df = pd.DataFrame(ids, columns=columns)
+    art_csv = df.to_csv('VASE.csv', index=False)
 
